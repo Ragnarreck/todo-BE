bea9396b483ab2b34ddbe97ed7a946f7bca05cae
Argo <artwood30@gmail.com>
2018-12-27 18:50:10 +0200

    change mongodb env

--- database/mongo_db.js
+++ database/mongo_db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose
-    .connect(process.env.DB_MONGO_URL, { useNewUrlParser: true })
+    .connect(process.env.DATABASE_URI , { useNewUrlParser: true })
     .then(() => console.log('mongoose connected'))
     .catch((err) => console.log(err));
 
